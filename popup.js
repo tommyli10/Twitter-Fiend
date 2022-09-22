5477acf2661a209284f25eb7d5be0fc1d2b89061
tommy li <tommyli76278@gmail.com>
2022-09-22 00:49:53 -0700

    figured out how to post tweet using postman

---
+++ popup.js
@@ -0,0 +1,20 @@
+console.log('loaded')
+
+document.getElementById('submit').addEventListener('click', () => {
+    console.log('click')
+    //send a post request the old fashioned way to 'https://api.twitter.com/2/tweets'
+    fetch('https://api.twitter.com/2/tweets', {
+        method: 'POST',
+        headers: { 'Access-Control-Allow-Origin': '*' },
+        // headers: 'Authorization: OAuth oauth_consumer_key="H24jacYAEfMafe1Uitu3jEDzO",oauth_token="301875389-qpyhlceucRGkITCOQEw3uZaJ9Gf4VcsHknI6oQUd",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1663826915",oauth_nonce="3gIokAfOm2S",oauth_version="1.0",oauth_signature="KqHzUjJSy0nOhczSRljTT0U4cxM%3D"',
+        body: JSON.stringify({
+            text: "hello world"
+        })
+        //pass all the authentication info into the header
+        //in the body create a json object with the key of text adn the value of whateer we typed in the input box
+    })
+        .then(data => data.json())
+        .then(data => console.log(data))
+        .catch(err => console.log(err))
+})
+//{Authorization: OAuth oauth_consumer_key="H24jacYAEfMafe1Uitu3jEDzO",oauth_token="301875389-qpyhlceucRGkITCOQEw3uZaJ9Gf4VcsHknI6oQUd",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1663826915",oauth_nonce="3gIokAfOm2S",oauth_version="1.0",oauth_signature="KqHzUjJSy0nOhczSRljTT0U4cxM%3D"
